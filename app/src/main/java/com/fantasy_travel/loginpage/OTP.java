9f0bafb0796f3f5f7fdc64731c7accd33bee83cd
iamani123 <iamani123@users.noreply.github.com>
2019-02-21 17:22:44 +0000

    added OTP class and made entry in manifest file.

---
+++ app/src/main/java/com/fantasy_travel/loginpage/OTP.java
@@ -0,0 +1,14 @@
+package com.fantasy_travel.loginpage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_input);
+    }
+}
+
