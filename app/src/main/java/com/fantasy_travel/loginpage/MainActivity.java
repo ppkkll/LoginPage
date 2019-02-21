5b897e49d150b9a9598f0b533cb7bf1ce1758b55
ppkkll <chenx4@tcd.ie>
2019-02-21 16:54:18 +0000

    Switch activity from Main to register finished

--- app/src/main/java/com/fantasy_travel/loginpage/MainActivity.java
+++ app/src/main/java/com/fantasy_travel/loginpage/MainActivity.java
@@ -1,17 +1,60 @@
 package com.fantasy_travel.loginpage;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity  {
+    Button sBtm,lBtm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // init();
 
-//onClick listener on buttom for switch activities should be implement here.
+        Button button = findViewById(R.id.signupButton);
+
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+
+
+    /*private void init() {
+        ;
+        sBtm=findViewById(R.id.signupButton);
+        lBtm=findViewById(R.id.loginButton);
+        sBtm.setOnClickListener(this);
+        lBtm.setOnClickListener(this);
+    }*/
+
+ /*   public void signUpBtm(View view){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 
+    public void loginBtm(View view){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
 
-}
+    @Override
+    public void onClick (View view){
+        switch (view.getId()){
+            case R.id.signupButton:
+        }
+
+    }
+}*/
--- app/src/main/java/com/fantasy_travel/loginpage/Register.java
+++ app/src/main/java/com/fantasy_travel/loginpage/Register.java
@@ -1,6 +1,49 @@
 package com.fantasy_travel.loginpage;
 
-public class Register {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
+public class Register extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button signupBtm;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
+
+       /* getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
+
+
+
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+//        signupBtm  = findViewById(R.id.signupBtm);
+
+        signupBtm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg("Login buttom hit!");
+            }
+        });
+
+
+
+
+    }
+    private void msg(String s)
+    {
+        Log.d("My App" , "LoginActivity" + "#######" + s);
+
+    }
+}
+*/
