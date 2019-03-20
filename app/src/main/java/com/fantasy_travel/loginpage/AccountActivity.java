296ad367226155693735c2ae6781ae400f37ccca
ppkkll <chenx4@tcd.ie>
2019-03-20 17:46:21 +0000

    Account Page

--- app/src/main/java/com/fantasy_travel/loginpage/AccountActivity.java
+++ app/src/main/java/com/fantasy_travel/loginpage/AccountActivity.java
@@ -1,13 +1,51 @@
 package com.fantasy_travel.loginpage;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class AccountActivity extends AppCompatActivity{
+    EditText etUsername;
+    EditText etPhone;
+
+    Button btnChangePass;
+    Button btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
 
+
+        btnChangePass = findViewById(R.id.btnChangePass);
+        btnSave = findViewById(R.id.savebtn);
+
+
+        btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( AccountActivity.this, ResetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( AccountActivity.this, ResetPasswordActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+
     }
+
 
 }
--- app/src/main/java/com/fantasy_travel/loginpage/ResetPasswordActivity.java
+++ app/src/main/java/com/fantasy_travel/loginpage/ResetPasswordActivity.java
@@ -1,14 +1,46 @@
 package com.fantasy_travel.loginpage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+    EditText etPass;
+    String Pass;
+    EditText etConfirmPass;
+    String ConfirmPass;
+    Button btnSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_resetpassword );
+
+            btnSubmit = findViewById(R.id.FP_Submit);
+
+            btnSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+             etPass = findViewById(R.id.FP_NewPass);
+             Pass = etPass.getText().toString();
+
+             etConfirmPass = findViewById(R.id.FP_ConfirmPass);
+             ConfirmPass = etConfirmPass.getText().toString();
+
+             if (Pass == ConfirmPass){
+                 Intent intent = new Intent( ResetPasswordActivity.this, LoginActivity.class);
+                 startActivity(intent);
+
+             }
+
+
+
+                }
+            });
 
     }
 }
