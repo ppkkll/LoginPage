998798d03af2c5ed20d24a890177ed0c6a64bf14
ppkkll <chenx4@tcd.ie>
2019-03-20 14:43:04 +0000

    Dashboard(sidebar)

--- app/src/main/java/com/fantasy_travel/loginpage/LoginActivity.java
+++ app/src/main/java/com/fantasy_travel/loginpage/LoginActivity.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     //EditText etUsername, etPassword;
--- app/src/main/java/com/fantasy_travel/loginpage/MainActivity.java
+++ app/src/main/java/com/fantasy_travel/loginpage/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity  {
         testBtm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( MainActivity.this, SidebarActivity.class);
+                Intent intent = new Intent( MainActivity.this, AccountActivity.class);
                 startActivity(intent);
             }
         });
