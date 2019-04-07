8d0a5ecd9452b9bf31208d02c188ffacf71ce282
ppkkll <chenx4@tcd.ie>
2019-04-07 14:29:49 +0100

    Side bar in Account Activity, DC,Create Plan and view Plan

--- app/src/main/java/com/fantasy_travel/loginpage/DailyCommuteCreatePlan.java
+++ app/src/main/java/com/fantasy_travel/loginpage/DailyCommuteCreatePlan.java
@@ -7,8 +7,11 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -55,12 +58,81 @@ public class DailyCommuteCreatePlan extends AppCompatActivity {
     public String start[], dest[];
     private String apiKey = "AIzaSyAj76-gXJ5gvXrjp12YlLA90xEokK7O234";
     private String username = "";
-
+    private DrawerLayout mDrawerLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_commute_create_plan);
+        mDrawerLayout = findViewById(R.id.Map_Drawer);
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                });
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        int id = menuItem.getItemId();
+
+                        switch (id){
+                            case R.id.nav_account:
+                                Toast.makeText(getApplicationContext(),"Account",Toast.LENGTH_SHORT).show();
+                                Intent intent_Account = new Intent( DailyCommuteCreatePlan.this, AccountActivity.class);
+                                startActivity(intent_Account);
+                                mDrawerLayout.closeDrawers();
+                                break;
+                            case R.id.nav_Daily_Commute:
+                                Intent intent_DC = new Intent( DailyCommuteCreatePlan.this, DailyCommuteViewPlan.class);
+                                startActivity(intent_DC);
+                                Toast.makeText(getApplicationContext(),"DailyCommute",Toast.LENGTH_SHORT).show();
+                                mDrawerLayout.closeDrawers();
+                                break;
+                            case R.id.nav_Find_Fellow_Traveller:
+                                Intent intent_FFT = new Intent( DailyCommuteCreatePlan.this, Maps.class);
+                                startActivity(intent_FFT);
+                                Toast.makeText(getApplicationContext(),"FindFellowTraveller",Toast.LENGTH_SHORT).show();
+                                mDrawerLayout.closeDrawers();
+                                break;
+                            case R.id.nav_Setting:
+                                Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_SHORT).show();
+                                Intent intent_Setting = new Intent( DailyCommuteCreatePlan.this, AccountActivity.class);
+                                startActivity(intent_Setting);
+                                break;
+                            case R.id.nav_LogOut:
+                                Toast.makeText(getApplicationContext(),"LouOut",Toast.LENGTH_SHORT).show();
+                                finish();
+                        }
+                        return true;
+                    }
+                });
+
 
         Places.initialize(getApplicationContext(), apiKey);
         PlacesClient placesClient = Places.createClient(this);
