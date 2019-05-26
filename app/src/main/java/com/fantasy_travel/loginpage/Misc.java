27b93c2c96add2669510863286e725ac6b57440f
Shubham <spandey@tcd.ie>
2019-05-26 22:43:53 +0100

    Update Misc.java

--- app/src/main/java/com/fantasy_travel/loginpage/Misc.java
+++ app/src/main/java/com/fantasy_travel/loginpage/Misc.java
@@ -5,4 +5,5 @@ public class Misc {
         public static String Url1 = "http://load1-467103352.eu-west-1.elb.amazonaws.com:8080";
         public static String Url2 = "http://load1-467103352.eu-west-1.elb.amazonaws.com:8082";
         public static String Url3 = "http://load1-467103352.eu-west-1.elb.amazonaws.com:8083";
+        public static String Url4 = "http://load1-467103352.eu-west-1.elb.amazonaws.com:8084";
 }
